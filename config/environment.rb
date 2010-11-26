9e1a140aa9d6b8ba032f31dfbdd1c17603163d59
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-11-26 20:53:55 -0200

    Update version and discourage sudo for installing gems

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,10 @@
-# Be sure to restart your server when you modify this file
+RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.gem 'warden', :version => '0.10.7'
-  config.gem 'devise', :version => '1.0.8'
+  config.gem 'devise', :version => '1.0.9'
   config.gem 'rails-footnotes', :version => '3.6.6'
 
   config.time_zone = 'UTC'
